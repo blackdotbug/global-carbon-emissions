a98ab22f3c9385043b7863ac95447a686e36d067
Heather Bree <me@justheatherbree.com>
2024-05-17 10:47:53 -0700

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import MainFullVh from "$lib/components/MainFullVh.svelte"
+  import Middle from "$lib/components/Middle.svelte"
+</script>
+
+<MainFullVh class="p-4">
+  <Middle class="p-8 border rounded-xl">
+    <h1 class="text-3xl">Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  </Middle>
+</MainFullVh>
